fd2f45fcc0207f448864b55629db6b275d95814d
melikeuckol <melikeerdodu2@gmail.com>
2026-06-28 20:16:15 +0300

    Initial project setup for QACraft

---
+++ src/main/java/com/example/qacraft_test_management/QacraftTestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.qacraft_test_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QacraftTestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QacraftTestManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qacraft_test_management/QacraftTestManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qacraft_test_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QacraftTestManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
